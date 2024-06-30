0c6d32ec8567df0dd5dd49559219c6d9e675b8c3
withJihyuk <lead@sver.dev>
2024-07-01 02:55:14 +0900

    :art: :: 불필요한 임포트 제거

--- lib/view/onBoarding/login_page.dart
+++ lib/view/onBoarding/login_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
-import 'package:trade_diary/view/components/button.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
