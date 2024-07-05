f6b4b99796e5beb19f74f6585062afdcf2cb4388
withJihyuk <lead@sver.dev>
2024-07-05 19:18:53 +0900

    :sparkles: :: 여전히 퍼블리싱중

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
@@ -18,49 +17,79 @@ class HomePage extends StatelessWidget {
               children: [
                 const Text(
                   '나의 일기',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
                 ),
                 const SizedBox(height: 30),
                 const Text(
                   '작년 이맘때,\n여행 일기에요 ✈️',
                   style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
                 ),
-                const SizedBox(height: 10),
+                const SizedBox(height: 12),
                 Container(
                     decoration: BoxDecoration(
                         color: Colors.indigoAccent[700],
                         borderRadius: BorderRadius.circular(14)),
-                    width: 300,
-                    height: 200,
-                    child: const Padding(
-                      padding: EdgeInsets.all(30),
-                      child: Row(
-                        children: [
-                          Column(
+                    width: 400,
+                    height: 150,
+                    child: Padding(
+                        padding: const EdgeInsets.all(28),
+                        child: Row(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text(
-                                '#오사카 #도쿄 #후쿠오카',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w400),
+                              const Row(
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        '#오사카 #도쿄 #후쿠오카',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w400),
+                                      ),
+                                      SizedBox(height: 4),
+                                      Text(
+                                        '일본 여행 중\n가장 잊지 못할 추억',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 24,
+                                            height: 1.3,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                    ],
+                                  ),
+                                ],
                               ),
-                              SizedBox(height: 4),
-                              Text(
-                                '일본 여행 중\n가장 잊지 못할 추억',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 24,
-                                    height: 1.3,
-                                    fontWeight: FontWeight.w600),
+                              const SizedBox(
+                                width: 12,
                               ),
-                              SizedBox(height: 10),
-                            ],
-                          )
-                        ],
+                              Container(
+                                decoration: const BoxDecoration(
+                                    color: Colors.black,
+                                    shape: BoxShape.circle),
+                                width: 110,
+                                height: 120,
+                              ),
+                            ]))),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        '교환 한 일기',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w600),
                       ),
-                    ))
+                      TextButton(
+                          onPressed: () {
+                            GoRouter.of(context).go('/exchange');
+                          },
+                          child: const Text('더보기'))
+                    ])
               ],
             )),
       )),
