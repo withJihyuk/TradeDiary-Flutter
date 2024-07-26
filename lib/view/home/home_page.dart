7f6d4c8c771d49af5b9051d2b2121f26d8fc05a0
withJihyuk <lead@sver.dev>
2024-07-26 23:40:41 +0900

    feat :: 일부 내용 수정

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -5,18 +5,48 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
           child: SingleChildScrollView(
         child: Padding(
-            padding: EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
             child:
                 Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Text('교환일기',
+              const Text('교환일기',
                   style: TextStyle(
                       fontSize: 24,
                       fontFamily: 'EF_Diary',
                       color: Colors.black)),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Row(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(20),
+                      width: 160,
+                      height: 80,
+                      decoration: const BoxDecoration(
+                        color: Color(0xFFE9F4EA),
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      child: const Text("일기 작성하기"),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(20),
+                      width: 160,
+                      height: 80,
+                      decoration: const BoxDecoration(
+                        color: Color(0xFFEEEEFE),
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      child: const Text("일기 작성하기"),
+                    )
+                  ],
+                ),
+              ),
             ])),
       )),
     );
