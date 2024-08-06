fc05f1ab676a496eae286347df346648a78913ad
Jihyuk Lee <lead@sver.dev>
2024-08-07 01:03:09 +0900

    feat :: 메인 페이지 디자인 수정

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -5,50 +5,122 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var screenWidth = MediaQuery.of(context).size.width;
+    var screenPadding = MediaQuery.of(context).padding.horizontal + 28;
+
     return Scaffold(
-      body: SafeArea(
-          child: SingleChildScrollView(
-        child: Padding(
-            padding: const EdgeInsets.all(20),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              const Text('교환일기',
-                  style: TextStyle(
-                      fontSize: 24,
-                      fontFamily: 'EF_Diary',
-                      color: Colors.black)),
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Row(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(20),
-                      width: 160,
-                      height: 80,
-                      decoration: const BoxDecoration(
-                        color: Color(0xFFE9F4EA),
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                      ),
-                      child: const Text("일기 작성하기"),
+        backgroundColor: const Color(0xFFF2F4F6),
+        body: SafeArea(
+            child: SingleChildScrollView(
+          child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(children: [
+                Container(
+                    padding: const EdgeInsets.all(26),
+                    width: screenWidth - screenPadding,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(18)),
                     ),
-                    const SizedBox(
-                      width: 10,
+                    child: const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "오늘의 일기",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 24),
+                            ),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              size: 20,
+                              color: Colors.grey,
+                            )
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        Row(
+                          children: [
+                            Row(
+                              children: [
+                                Icon(Icons.calendar_month,
+                                    size: 40, color: Colors.blueAccent),
+                                SizedBox(width: 14),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text("2021년 10월 10일",
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w500,
+                                            color: Colors.grey)),
+                                    Text("오늘의 일기가 없습니다.",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: 18))
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    )),
+                const SizedBox(height: 20),
+                Container(
+                    padding: const EdgeInsets.all(26),
+                    width: screenWidth - screenPadding,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(18)),
                     ),
-                    Container(
-                      padding: const EdgeInsets.all(20),
-                      width: 160,
-                      height: 80,
-                      decoration: const BoxDecoration(
-                        color: Color(0xFFEEEEFE),
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                      ),
-                      child: const Text("일기 작성하기"),
-                    )
-                  ],
-                ),
-              ),
-            ])),
-      )),
-    );
+                    child: const Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "칭찬스티커",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 24),
+                            ),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              size: 20,
+                              color: Colors.grey,
+                            )
+                          ],
+                        ),
+                      ],
+                    )),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                //   children: [
+                //     Container(
+                //       padding: const EdgeInsets.all(20),
+                //       width: screenWidth / 2 - screenPadding,
+                //       height: 80,
+                //       decoration: const BoxDecoration(
+                //         color: Color(0xFFE9F4EA),
+                //         borderRadius: BorderRadius.all(Radius.circular(10)),
+                //       ),
+                //       child: const Text("일기 작성하기"),
+                //     ),
+                //     Container(
+                //       padding: const EdgeInsets.all(20),
+                //       width: screenWidth / 2 - screenPadding,
+                //       height: 80,
+                //       decoration: const BoxDecoration(
+                //         color: Color(0xFFEEEEFE),
+                //         borderRadius: BorderRadius.all(Radius.circular(10)),
+                //       ),
+                //       child: const Text("일기 작성하기"),
+                //     )
+                //   ],
+                // )
+              ])),
+        )));
   }
 }
--- lib/view/onBoarding/start_page.dart
+++ lib/view/onBoarding/start_page.dart
@@ -8,31 +8,34 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      const SizedBox(height: 40),
-      const Text(
-        "럭키비키",
-        textAlign: TextAlign.center,
-        style: TextStyle(fontFamily: 'EF_Diary', fontSize: 38, height: 1.2),
-      ),
-      const SizedBox(height: 4),
-      const Text(
-        "남들과 함께 하여 더 즐겁게 일기를 써봐요 ",
-        style: TextStyle(fontSize: 18),
-      ),
-      SvgPicture.asset(
-        'assets/images/hamster.svg',
-        width: 500,
-        height: 500,
-      ),
-      const SizedBox(height: 20),
-      const Button(
-        buttonName: '시작하기',
-        route: '/login',
-        isRoute: true,
-      ),
-    ])));
+        body: SafeArea(
+            child: SingleChildScrollView(
+                child: Center(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+          const SizedBox(height: 40),
+          const Text(
+            "럭키비키",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontFamily: 'EF_Diary', fontSize: 38, height: 1.2),
+          ),
+          const SizedBox(height: 4),
+          const Text(
+            "남들과 함께 하여 더 즐겁게 일기를 써봐요 ",
+            style: TextStyle(fontSize: 18),
+          ),
+          SvgPicture.asset(
+            'assets/images/hamster.svg',
+            width: 500,
+            height: 500,
+          ),
+          const SizedBox(height: 20),
+          const Button(
+            buttonName: '시작하기',
+            route: '/login',
+            isRoute: true,
+          ),
+        ])))));
   }
 }
