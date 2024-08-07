d8a2f5bd63a260aa98de98e9babd3d8c299f8c12
Jihyuk Lee <lead@sver.dev>
2024-08-07 12:36:26 +0900

    feat :: 마진 값 변경을 통한 UX 개선

--- lib/view/onBoarding/start_page.dart
+++ lib/view/onBoarding/start_page.dart
@@ -14,7 +14,7 @@ class FirstPage extends StatelessWidget {
                     child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-          const SizedBox(height: 40),
+          const SizedBox(height: 80),
           const Text(
             "럭키비키",
             textAlign: TextAlign.center,
@@ -22,15 +22,14 @@ class FirstPage extends StatelessWidget {
           ),
           const SizedBox(height: 4),
           const Text(
-            "남들과 함께 하여 더 즐겁게 일기를 써봐요 ",
+            "친구와 교환하며 일기를 작성해봐요",
             style: TextStyle(fontSize: 18),
           ),
           SvgPicture.asset(
             'assets/images/hamster.svg',
-            width: 500,
-            height: 500,
+            width: 400,
+            height: 400,
           ),
-          const SizedBox(height: 20),
           const Button(
             buttonName: '시작하기',
             route: '/login',
