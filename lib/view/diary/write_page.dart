70bf8be6dcf6c53fcac5f56f01d73a0303308749
Jihyuk Lee <lead@sver.dev>
2024-08-16 12:33:56 +0900

    feat :: 색상 롤백

--- lib/view/diary/write_page.dart
+++ lib/view/diary/write_page.dart
@@ -54,9 +54,9 @@ class WritePage extends StatelessWidget {
                 decoration: InputDecoration(
                   hintText: "오늘은 어떤 일이 있으셨나요?\n자유롭게 일기를 작성해봐요!",
                   hintStyle: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400,
-                      color: DiaryColorBlue.normalActive),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                  ),
                   border: InputBorder.none,
                 ),
                 style: TextStyle(fontSize: 18),
