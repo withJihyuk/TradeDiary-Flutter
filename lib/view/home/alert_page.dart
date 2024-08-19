ec8283709d68d74caa2ae3d22d4fe08f006130ac
Jihyuk Lee <lead@sver.dev>
2024-08-19 12:17:08 +0900

    feat :: 알림 페이지 기본 레이아웃

--- lib/view/home/alert_page.dart
+++ lib/view/home/alert_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trade_diary/view/components/global_appbar.dart';
 
 class AlertPage extends StatefulWidget {
   const AlertPage({super.key});
@@ -11,9 +12,13 @@ class _AlertPageState extends State<AlertPage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text("Alert Page"),
-      ),
+      appBar: GlobalAppbar(title: "알림"),
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      )),
     );
   }
 }
