36f13a29401bf6be9acfdd77faf764043a518122
Jihyuk Lee <lead@sver.dev>
2024-12-12 23:18:26 +0900

    feat :: 애플 로그인 구현 준비

--- lib/viewModel/oauth_model.dart
+++ lib/viewModel/oauth_model.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'dart:io';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:crypto/crypto.dart';
 
 final supabase = Supabase.instance.client;
 
@@ -34,6 +37,31 @@ class OauthViewModel {
         return null;
       }
     }
+  }
+
+  Future<AuthResponse> signInWithApple() async {
+    final rawNonce = supabase.auth.generateRawNonce();
+    final hashedNonce = sha256.convert(utf8.encode(rawNonce)).toString();
+
+    final credential = await SignInWithApple.getAppleIDCredential(
+      scopes: [
+        AppleIDAuthorizationScopes.email,
+        AppleIDAuthorizationScopes.fullName,
+      ],
+      nonce: hashedNonce,
+    );
+
+    final idToken = credential.identityToken;
+    if (idToken == null) {
+      throw const AuthException(
+          'Could not find ID Token from generated credential.');
+    }
+
+    return supabase.auth.signInWithIdToken(
+      provider: OAuthProvider.apple,
+      idToken: idToken,
+      nonce: rawNonce,
+    );
   }
 
   webGoogleLogin() {
