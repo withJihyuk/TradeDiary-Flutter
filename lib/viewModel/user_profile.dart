4be3bcea56945cadcef3719653ee2489331c3721
Jihyuk Lee <lead@sver.dev>
2024-12-16 11:27:31 +0900

    delete :: 필요 없는 파일 제거

--- lib/viewModel/user_profile.dart
+++
@@ -1,8 +0,0 @@
-import 'package:trade_diary/repository/user_profile.dart';
-
-class UserProfileViewModel {
-  final repo = UserProfileRepo();
-  Future<List> getUserProfile(String userId) {
-    return repo.getUserProfile(userId);
-  }
-}
