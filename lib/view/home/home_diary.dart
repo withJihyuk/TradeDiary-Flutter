239d76a8f576fc9124705699827d6deb3524c709
Jihyuk Lee <lead@sver.dev>
2024-12-16 11:10:26 +0900

    style :: 디자인 세부사항 반영
    월 별 표기는 추후 구현

--- lib/view/home/home_diary.dart
+++ lib/view/home/home_diary.dart
@@ -1,10 +1,47 @@
 part of '../home/home_page.dart';
 
 class _DiaryList extends StatelessWidget {
-  const _DiaryList({super.key});
+  const _DiaryList({super.key, required this.diaryList});
+  final List<DiaryPostModel> diaryList;
 
   @override
   Widget build(BuildContext context) {
-    return const Text("diary list");
+    return ListView.builder(
+        shrinkWrap: true,
+        primary: false,
+        itemCount: diaryList.isEmpty ? 1 : diaryList.length,
+        itemBuilder: (context, index) {
+          if (diaryList.isEmpty) {
+            return const Center(
+              child: Text("일기가 없습니다."),
+            );
+          }
+          return ListTile(
+              contentPadding: const EdgeInsets.all(0),
+              minVerticalPadding: 0,
+              title: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  child: Column(children: [
+                    Column(
+                      children: [
+                        DiaryHomeContentRead(
+                            contentName: diaryList[index].subject,
+                            contentDate:
+                                "${diaryList[index].date.month}/${diaryList[index].date.day}",
+                            contentPreview: diaryList[index].content,
+                            contentImage: diaryList[index].image),
+                        const SizedBox(height: 24),
+                        (diaryList.length == index + 1)
+                            ? const SizedBox(
+                                height: 1,
+                              )
+                            : const Divider(
+                                height: 1,
+                                color: DiaryMainGrey.grey50,
+                              ) // 월별 표기 필요
+                      ],
+                    ),
+                  ])));
+        });
   }
 }
--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:trade_diary/desginSystem/color.dart';
 import 'package:trade_diary/desginSystem/fontsize.dart';
+import 'package:trade_diary/model/diary_post.dart';
+import 'package:trade_diary/view/components/diary_home_content_read.dart';
 
 part '../home/home_scaffold.dart';
 part '../home/home_header.dart';
@@ -17,10 +19,25 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const _Scaffold(
-      header: _Header(),
-      searchBox: _SearchBox(),
-      diaryList: _DiaryList(),
+    return _Scaffold(
+      header: const _Header(),
+      searchBox: const _SearchBox(),
+      diaryList: _DiaryList(
+        diaryList: [
+          DiaryPostModel(
+              content: "as",
+              subject: "asdf",
+              date: DateTime(2024, 12, 10),
+              image: "adsf",
+              isPrivate: false),
+          DiaryPostModel(
+              content: "감자튀김이 좋아요감자튀김이 좋아요감자튀김이 좋아요감자튀김이 좋아요감자튀김이 좋아요",
+              subject: "감자튀김이 좋아요",
+              date: DateTime(2024, 12, 25),
+              image: "adsf",
+              isPrivate: false)
+        ],
+      ),
     );
   }
 }
