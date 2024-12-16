281bd2be3c40b17a330ddf08cdc7f8c6b206eb2e
Jihyuk Lee <lead@sver.dev>
2024-12-16 09:18:58 +0900

    chore :: 폰트 관련 디자인과 매치

--- lib/desginSystem/fontsize.dart
+++ lib/desginSystem/fontsize.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 
 abstract class AppTextStyle {
   static TextStyle pretendardStyle(double size, double? height) => TextStyle(
-        fontFamily: 'pretendard',
-        leadingDistribution: TextLeadingDistribution.even,
-        letterSpacing: 0,
+        fontFamily: 'Pretendard',
         fontSize: size,
         height: height,
       );
@@ -19,20 +17,20 @@ abstract class AppTextStyle {
         fontWeight: FontWeight.w600,
       );
 
-  static final TextStyle labelRegular = pretendardRegularStyle(14, 21);
-  static final TextStyle labelSemi = pretendardSemiBoldStyle(14, 21);
-  static final TextStyle m3Regular = pretendardRegularStyle(14, 26.4);
-  static final TextStyle m3Semi = pretendardSemiBoldStyle(14, 26.4);
-  static final TextStyle m2Regular = pretendardRegularStyle(18, 27);
-  static final TextStyle m2Semi = pretendardSemiBoldStyle(18, 27);
-  static final TextStyle m1Regular = pretendardRegularStyle(20, 30);
-  static final TextStyle m1Semi = pretendardSemiBoldStyle(20, 30);
-  static final TextStyle h4Regular = pretendardRegularStyle(24, 31.2);
-  static final TextStyle h4Semi = pretendardSemiBoldStyle(24, 31.2);
-  static final TextStyle h3Regular = pretendardRegularStyle(28, 36.4);
-  static final TextStyle h3Semi = pretendardSemiBoldStyle(28, 36.4);
-  static final TextStyle h2Regular = pretendardRegularStyle(32, 38.4);
-  static final TextStyle h2Semi = pretendardSemiBoldStyle(32, 38.4);
-  static final TextStyle h1Regular = pretendardRegularStyle(36, 43.2);
-  static final TextStyle h1Semi = pretendardSemiBoldStyle(36, 43.2);
+  static final TextStyle labelRegular = pretendardRegularStyle(14, 1.5);
+  static final TextStyle labelSemi = pretendardSemiBoldStyle(14, 1.5);
+  static final TextStyle m3Regular = pretendardRegularStyle(16, 1.65);
+  static final TextStyle m3Semi = pretendardSemiBoldStyle(16, 1.65);
+  static final TextStyle m2Regular = pretendardRegularStyle(18, 1.5);
+  static final TextStyle m2Semi = pretendardSemiBoldStyle(18, 1.5);
+  static final TextStyle m1Regular = pretendardRegularStyle(20, 1.5);
+  static final TextStyle m1Semi = pretendardSemiBoldStyle(20, 1.5);
+  static final TextStyle h4Regular = pretendardRegularStyle(24, 1.3);
+  static final TextStyle h4Semi = pretendardSemiBoldStyle(24, 1.3);
+  static final TextStyle h3Regular = pretendardRegularStyle(28, 1.3);
+  static final TextStyle h3Semi = pretendardSemiBoldStyle(28, 1.3);
+  static final TextStyle h2Regular = pretendardRegularStyle(32, 1.2);
+  static final TextStyle h2Semi = pretendardSemiBoldStyle(32, 1.2);
+  static final TextStyle h1Regular = pretendardRegularStyle(36, 1.2);
+  static final TextStyle h1Semi = pretendardSemiBoldStyle(36, 1.2);
 }
