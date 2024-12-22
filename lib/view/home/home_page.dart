ed29f6e258d6c3b1cc28f748f0c2ceec255dace1
Jihyuk Lee <lead@sver.dev>
2024-12-22 23:11:49 +0900

    style :: 홈 이미지 배치

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:trade_diary/desginSystem/color.dart';
 
 class HomePage extends StatefulWidget {
@@ -27,56 +26,48 @@ class _HomePageState extends State<HomePage> {
             width: double.infinity,
             height: double.infinity,
             child: SafeArea(
-                child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 32.w, vertical: 38.h),
-              child: Column(
-                children: [
-                  Padding(
-                      padding: EdgeInsets.only(top: 66.h, left: 213.w),
-                      child: Image.asset("assets/images/character/sun.png",
-                          width: 132.w, height: 135.h)),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Image.asset("assets/images/character/cloud.png",
-                        width: 102.w, height: 71.h),
-                  ),
-                  Padding(
-                      padding: EdgeInsets.only(top: 165.h),
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  const Text("LV.1"),
-                                  SizedBox(
-                                    width: 8.w,
-                                  ),
-                                  const Text("감자")
-                                ],
-                              ),
-                              const Text('0/12')
-                            ],
-                          ),
-                          SizedBox(
-                            height: 8.h,
-                          ),
-                          Container(
-                            width: double.infinity,
-                            height: 12.h,
-                            decoration: BoxDecoration(
-                                color: DiaryMainGrey.grey500,
-                                borderRadius: BorderRadius.circular(16)),
-                          ),
-                          SizedBox(
-                            height: 8.h,
-                          ),
-                          const Text("일기와 도전과제를 설정하면 감자가 성장해요")
-                        ],
-                      ))
-                ],
+                child: Column(children: [
+              Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 32.w, vertical: 38.h),
+                  child: Column(children: [
+                    Padding(
+                        padding: EdgeInsets.only(top: 66.h, left: 213.w),
+                        child: Image.asset("assets/images/character/sun.png",
+                            width: 132.w, height: 135.h)),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Image.asset("assets/images/character/cloud.png",
+                          width: 102.w, height: 71.h),
+                    ),
+                  ])),
+              Expanded(
+                child: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Positioned(
+                      top: 110.h,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: Container(
+                        decoration:
+                            const BoxDecoration(color: Color(0xFF826A56)),
+                      ),
+                    ),
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      child: Image.asset(
+                        "assets/images/character/img-potato-1lv.png",
+                        fit: BoxFit.cover,
+                        height: 166.h,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ))));
+            ]))));
   }
 }
