a566dfc4e920d02317049727c05a56a8aff6cd61
Jihyuk Lee <lead@sver.dev>
2024-12-26 15:52:11 +0900

    feat :: 약관 노션 링크 연결

--- lib/view/systemSetting/system_setting_page.dart
+++ lib/view/systemSetting/system_setting_page.dart
@@ -6,6 +6,7 @@ import 'package:trade_diary/router.dart';
 import 'package:trade_diary/view/components/setting_menu.dart';
 import 'package:trade_diary/view/components/top_navigation_bar.dart';
 import 'package:trade_diary/viewModel/oauth_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SystemSettingPage extends StatelessWidget {
   const SystemSettingPage({super.key});
@@ -23,14 +24,20 @@ class SystemSettingPage extends StatelessWidget {
                     SizedBox(height: 40.h),
                     TextSettingMenu(
                       menuName: "이용약관",
-                      onPressed: () {},
+                      onPressed: () {
+                        launchUrl(Uri.parse(
+                            'https://working-mailman-871.notion.site/1685f20bda6f80de9e6fd71653317e55'));
+                      },
                     ),
                     SizedBox(
                       height: 20.h,
                     ),
                     TextSettingMenu(
                       menuName: "개인정보 처리방침",
-                      onPressed: () {},
+                      onPressed: () {
+                        launchUrl(Uri.parse(
+                            'https://working-mailman-871.notion.site/1685f20bda6f80a3bc1fe4d5437772a7'));
+                      },
                     ),
                     SizedBox(
                       height: 32.h,
