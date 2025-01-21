89a1e80112c22bad898d03c4cdb8c40966c6894a
Jihyuk Lee <jihyuk1714@gmail.com>
2025-01-21 21:43:04 +0900

    Revert "🔀 :: (#30) - 캐릭터 레벨에 따른 이미지 교체"

--- lib/util/level.dart
+++ lib/util/level.dart
@@ -1,34 +1,21 @@
 class LevelSystem {
-  // 각 레벨에 필요한 누적 경험치
-  final List<int> levelExp = [15, 34, 57, 92, 135, 372];
+  final List<int> expThresholds = [15, 34, 57, 92, 135, 372];
+  final List<int> expRequired = [15, 34, 57, 92, 135, 372];
 
   int getLevel(int exp) {
-    for (int i = 0; i < levelExp.length; i++) {
-      if (exp < levelExp[i]) {
-        return i + 1;
+    for (int i = 1; i < expThresholds.length; i++) {
+      if (exp < expThresholds[i]) {
+        return i;
       }
     }
-    return levelExp.length + 1;
+    return expThresholds.length;
   }
 
   int expToNextLevel(int exp) {
     int currentLevel = getLevel(exp);
-    if (currentLevel > levelExp.length) {
+    if (currentLevel >= expThresholds.length) {
       return 0;
     }
-    return levelExp[currentLevel - 1] -
-        (currentLevel == 1 ? 0 : levelExp[currentLevel - 2]);
-  }
-
-  int getCurrentLevelExp(int level) {
-    if (level <= 1) return 0;
-    if (level > levelExp.length + 1) return levelExp.last;
-    return levelExp[level - 2];
-  }
-
-  int getRemainingExp(int currentExp) {
-    int currentLevel = getLevel(currentExp);
-    if (currentLevel > levelExp.length) return 0;
-    return levelExp[currentLevel - 1] - currentExp;
+    return expRequired[currentLevel + 1];
   }
 }
