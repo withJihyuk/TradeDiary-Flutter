74c101244034594d4ed81ee4ee3651d548cb56c5
Jihyuk Lee <lead@sver.dev>
2025-04-29 13:53:36 +0900

    feat :: 화면 전환 페이드 효과 적용

--- lib/view/game/swipe_game_guide_3.dart
+++ lib/view/game/swipe_game_guide_3.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:go_router/go_router.dart';
 import 'package:trade_diary/designSystem/fontsize.dart';
 import 'package:trade_diary/view/components/button.dart';
 import 'package:trade_diary/view/game/swipe_game.dart';
+import 'package:trade_diary/view/game/swipe_game_guide_1.dart';
 
-class SwipeGameGuide3 extends StatelessWidget {
+class SwipeGameGuide3 extends StatefulWidget {
   const SwipeGameGuide3({super.key});
+
+  @override
+  State<SwipeGameGuide3> createState() => _SwipeGameGuide3State();
+}
+
+class _SwipeGameGuide3State extends State<SwipeGameGuide3> {
+  bool _isInitialized = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (!_isInitialized) {
+      // 게임 화면의 이미지 프리로드
+      precacheImage(const AssetImage("assets/images/character/grass.png"), context);
+      precacheImage(const AssetImage("assets/images/character/potato-with-swipegame.png"), context);
+      _isInitialized = true;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,17 +62,38 @@ class SwipeGameGuide3 extends StatelessWidget {
               SizedBox(
                   width: 350.w,
                   child: DiaryButton(
-                      text: "시작하기", onPressed: () => context.go('/swipeGame'))),
+                      text: "시작하기", 
+                      onPressed: () => Navigator.of(context).pushReplacement(
+                        PageRouteBuilder(
+                          pageBuilder: (context, animation, secondaryAnimation) => const SwipeGame(),
+                          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                            return FadeTransition(
+                              opacity: animation,
+                              child: child,
+                            );
+                          },
+                          transitionDuration: const Duration(milliseconds: 300),
+                        ),
+                      ))),
               SizedBox(
                 height: 18.h,
               ),
               GestureDetector(
-                onTap: () => context.go('/swipeGameGuide1'),
-                child:
-                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                onTap: () => Navigator.of(context).pushReplacement(
+                  PageRouteBuilder(
+                    pageBuilder: (context, animation, secondaryAnimation) => const SwipeGameGuide1(),
+                    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                      return FadeTransition(
+                        opacity: animation,
+                        child: child,
+                      );
+                    },
+                    transitionDuration: const Duration(milliseconds: 300),
+                  ),
+                ),
+                child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
                   Text("다시보기",
-                      style:
-                          AppTextStyle.m3Regular.copyWith(color: Colors.white)),
+                      style: AppTextStyle.m3Regular.copyWith(color: Colors.white)),
                   SvgPicture.asset("assets/images/icons/repeat.svg")
                 ]),
               )
